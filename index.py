cf098e9711d940ced5f68905120e0f8ca8a1a61a
lexBenji <discordo12341@outlook.com>
2023-02-19 19:56:32 +0700

    added files

---
+++ index.py
@@ -0,0 +1,194 @@
+from getch import getch
+from stdio import *
+
+board = [
+'11111111',
+'10000001',
+'10000001',
+'10000001',
+'10000001',
+'10000001',
+'100000B1',
+'11111111'
+]
+
+facing = 'up'
+
+y = 1
+x = 1
+py = y
+px = x
+
+br = [list(x) for x in board]
+
+while True:
+    write('\033c')
+    br[py][x] = br[py][x].replace('2','0')
+    br[y][px] = br[y][px].replace('2','0')
+    br[y][x] = '2'
+    writeln(facing)
+    writeln(y,' ',x)
+
+    if facing == 'up':
+        if br[y-1][x] == '1':
+            writeln('#')
+        elif br[y-1][x] == 'B':
+            writeln('B')
+        else:
+            writeln('/')
+    elif facing == 'down':
+        if br[y+1][x] == '1':
+            writeln('#')
+        elif br[y+1][x] == 'B':
+            writeln('B')
+        else:
+            writeln('/')
+    elif facing == 'left':
+        if br[y][x-1] == '1':
+            writeln('#')
+        elif br[y][x-1] == 'B':
+            writeln('B')
+        else:
+            writeln('/')
+    elif facing == 'right':
+        if br[y][x+1] == '1':
+            writeln('#')
+        elif br[y][x+1] == 'B':
+            writeln('B')
+        else:
+            writeln('/')
+
+    for i in br:
+        for j in i:
+            if j == '1':
+                write('#')
+            elif j == '0':
+                write(' ')
+            elif j == '2':
+                write('@')
+            elif j == 'B':
+                write('B')
+        writeln()
+
+    key = getch().lower()
+
+    if key == 'x':
+        break
+    elif key == 'w':
+        if facing == 'up':
+            if br[y - 1][x] == '1' or br[y-1][x] == 'B':
+                pass
+            else:
+                py = y
+                y = y - 1
+        elif facing == 'down':
+            if br[y + 1][x] == '1' or br[y+1][x] == 'B':
+                pass
+            else:
+                py = y
+                y = y + 1
+        elif facing == 'right':
+            if br[y][x + 1] == '1' or br[y][x+1] == 'B':
+                pass
+            else:
+                px = x
+                x = x + 1
+        elif facing == 'left':
+            if br[y][x - 1] == '1' or br[y][x-1] == 'B':
+                pass
+            else:
+                px = x
+                x = x - 1
+    elif key == 's':
+        if facing == 'up':
+            if br[y + 1][x] == '1' or br[y+1][x] == 'B':
+                pass
+            else:
+                py = y
+                y = y + 1
+        elif facing == 'down':
+            if br[y - 1][x] == '1' or br[y-1][x] == 'B':
+                pass
+            else:
+                py = y
+                y = y - 1
+        elif facing == 'right':
+            if br[y][x - 1] == '1' or br[y][x-1] == 'B':
+                pass
+            else:
+                px = x
+                x = x - 1
+        elif facing == 'left':
+            if br[y][x + 1] == '1' or br[y][x+1] == 'B':
+                pass
+            else:
+                px = x
+                x = x + 1
+    elif key == 'd':
+        if facing == 'up':
+            if br[y][x + 1] == '1' or br[y][x+1] == 'B':
+                pass
+            else:
+                px = x
+                x = x + 1
+        elif facing == 'down':
+            if br[y][x - 1] == '1' or br[y][x-1] == 'B':
+                pass
+            else:
+                px = x
+                x = x - 1
+        elif facing == 'right':
+            if br[y + 1][x] == '1' or br[y+1][x] == 'B':
+                pass
+            else:
+                py = y
+                y = y + 1
+        elif facing == 'left':
+            if br[y - 1][x] == '1' or br[y-1][x] == 'B':
+                pass
+            else:
+                py = y
+                y = y - 1
+    elif key == 'a':
+        if facing == 'up':
+            if br[y][x - 1] == '1' or br[y][x-1] == 'B':
+                pass
+            else:
+                px = x
+                x = x - 1
+        elif facing == 'down':
+            if br[y][x + 1] == '1' or br[y][x+1] == 'B':
+                pass
+            else:
+                px = x
+                x = x + 1
+        elif facing == 'right':
+            if br[y - 1][x] == '1' or br[y-1][x] == 'B':
+                pass
+            else:
+                py = y
+                y = y - 1
+        elif facing == 'left':
+            if br[y + 1][x] == '1' or br[y+1][x] == 'B':
+                pass
+            else:
+                py = y
+                y = y + 1
+    elif key == 'l':
+        if facing == 'up':
+            facing = 'right'
+        elif facing == 'down':
+            facing = 'left'
+        elif facing == 'right':
+            facing = 'down'
+        elif facing == 'left':
+            facing = 'up'
+    elif key == 'k':
+        if facing == 'up':
+            facing = 'left'
+        elif facing == 'down':
+            facing = 'right'
+        elif facing == 'right':
+            facing = 'up'
+        elif facing == 'left':
+            facing = 'down'
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Movement Testing
+
+`w a s d` to move around<br>
+`l` to turn right<br>
+`k` to turn left<br>
+<br>
+movement will be according to the way you're looking.
